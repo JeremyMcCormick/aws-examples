b064185500bd83ade1c458bfe7e06c0d96ddd135
Jeremy McCormick <jermccormick@gmail.com>
2019-03-08 15:12:30 +0700

    Add VPC setup (close to working now).

--- python/vpc_scenario_1.py
+++ python/vpc_scenario_1.py
@@ -3,19 +3,32 @@
 """
 Example: Amazon VPC
 Scenario 1: VPC with a Single Public Subnet
+https://docs.aws.amazon.com/vpc/latest/userguide/VPC_Scenario1.html
+
+@author Jeremy McCormick
 """
 
 import boto3
 from botocore.exceptions import ClientError
 
-client = boto3.client('ec2')
-ec2 = boto3.resource('ec2')
+import os
+
+# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html#client
+client = boto3.client('ec2', region_name = 'ap-southeast-1')
+
+ec2 = boto3.resource('ec2', region_name = 'ap-southeast-1')
+
+# examples
+# https://gist.github.com/nguyendv/8cfd92fc8ed32ebb78e366f44c2daea6
 
 try:
 
     # Create the VPC.
     resp = client.create_vpc(CidrBlock='10.0.0.0/16')
+    #print(resp)
     vpc_id = resp['Vpc']['VpcId']
+    vpc = ec2.Vpc(vpc_id)
+    vpc.wait_until_available()
 
     # Set name tag on VPC.
     # https://stackoverflow.com/questions/50108783/aws-sdk-how-to-set-the-vpc-name-tag-using-boto3
@@ -26,14 +39,16 @@ try:
     resp = client.create_subnet(
         CidrBlock='10.0.0.0/24',
         VpcId=vpc_id)
+    #print(resp)
     subnet_id = resp['Subnet']['SubnetId']
 
     # Create a SG.
-    data = client.create_security_group(
+    resp = client.create_security_group(
         Description='web server SG', GroupName='WebServerSG', VpcId=vpc_id)
-    group_id = data['GroupId']
+    #print(resp)
+    group_id = resp['GroupId']
 
-    # Add ingress rules to the SG.  The default outbound rules will allow all.
+    # Add ingress rules to the SG.  The default outbound rules will allow all traffic.
     # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/ec2-example-security-group.html
     resp = client.authorize_security_group_ingress(
         GroupId=group_id,
@@ -51,7 +66,37 @@ try:
              'ToPort': 22,
              'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
         ])
+    #print(resp)
+    
+    # Create an Internet Gateway.    
+    resp = client.create_internet_gateway()
+    gateway_id = resp['InternetGateway']['InternetGatewayId']
+    
+    # Attach the Internet Gatway to the VPC.
+    resp = client.attach_internet_gateway(InternetGatewayId=gateway_id, VpcId=vpc_id)
+        
+    # Create a route table.
+    resp = client.create_route_table(VpcId=vpc_id)
+    route_table_id = resp['RouteTable']['RouteTableId']
+    
+    # Get the route table.
+    route_table = ec2.RouteTable(route_table_id)
 
+    # Create a route to the IGW.
+    route = route_table.create_route(DestinationCidrBlock='0.0.0.0/0', GatewayId=gateway_id)
+
+    # Associate route table with subnet.
+    route_table.associate_with_subnet(SubnetId=subnet_id)
+          
+    # Create a key pair to use for SSHing into the instance.
+    # https://sdbrett.com/BrettsITBlog/2016/05/creating-aws-instances-with-boto3/
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/ec2-example-key-pairs.html    
+    outfile = open('MyKeyPair.pem', 'w')
+    key_pair = client.create_key_pair(KeyName='MyKeyPair')
+    key_pair_out = str(key_pair['KeyMaterial'])
+    outfile.write(key_pair_out)
+#    os.chmod(outfile.name, 400)
+    
     # Create an EC2 instance associated to the subnet.
     # https://stackoverflow.com/questions/32863768/how-to-create-an-ec2-instance-using-boto3
     resp = client.run_instances(
@@ -65,8 +110,9 @@ try:
                 }
             }
         ],
-        ImageId='ami-02da3a138888ced85',
+        ImageId='ami-00158b185c8cc09dc',        
         InstanceType='t2.micro',
+        KeyName='MyKeyPair',
         MaxCount=1,
         MinCount=1,        
         NetworkInterfaces=[
@@ -78,6 +124,7 @@ try:
             }
         ]
     )
+    #         ImageId='ami-02da3a138888ced85',
     
 except ClientError as e:
     print(e)
